59eb17e1a54df8cff419b25a15c5cd1adf63bffb
osama almousa <39463995+oalmousa@users.noreply.github.com>
2020-07-09 14:04:32 +0300

    first website upload

---
+++ contact/js/js2.js
@@ -0,0 +1,7 @@
+function opennav(){
+  document.getElementById('mysidenav-s').style.width="100%"; document.getElementById('main-s').style.width="100%";
+}
+
+function closenav(){
+  document.getElementById('mysidenav-s').style.width="0px"; document.getElementById('main-s').style.width="0px";
+}
